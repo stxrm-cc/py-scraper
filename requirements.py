7d6557f0b0a249ff1823fdda009900402262c1fe
s <44237108+stxrm-cc@users.noreply.github.com>
2023-04-28 10:26:49 +0200

    Add files via upload

--- requirements.py
+++ requirements.py
@@ -3,12 +3,13 @@ import subprocess
 import platform
 
 user_os = platform.system()
+err = 0
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 ret = '\n'.join(f"-> {req}" for req in requirements)
-print(f"Installing the following requirements: {ret}")
+print(f"Installing the following requirements: \n{ret}")
 
 if user_os == "Darwin":
     pip = "pip3"
@@ -16,13 +17,16 @@ elif user_os == "Linux" or user_os == "Windows":
     pip = "pip"
     
 for requirement in requirements:
+    print("====================================================================================")
     try:
         subprocess.check_call([pip, "install", requirement])
         # ugly if ure not running it by double clicking through file explorer
-        print("====================================================================================")
+        
     except subprocess.CalledProcessError as e:
+        err += 1
         print(e)
         continue
-
-input("/!\ Installing required modules done. Press Enter to exit.\t")
+        
+print("====================================================================================")
+input(f"/!\ Installing required modules finished with {err} errors. Press Enter to exit.\t")
 
